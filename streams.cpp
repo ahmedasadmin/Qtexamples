07bb334dab74273c30da84fa8d252ed0cb8734d1
ahmedasadmin <ahmednagi.hawk@gmail.com>
2023-11-27 20:11:47 +0200

    complete streams

--- streams.cpp
+++ streams.cpp
@@ -2,7 +2,6 @@
 #include <sstream>
 #include <fstream>
 
-
 int main(){
 
     using namespace std;
@@ -16,7 +15,10 @@ int main(){
 
 
     cout << "An in memory stream " << endl;
-    strbuf << "luckynumber: " << lucky << endl;
+    strbuf << "luckynumber: " << lucky << endl
+            << "PI: " << pi << endl 
+             << "e: " << e << endl;
+
     string strval = strbuf.str();
     cout << strval;
 
@@ -28,5 +30,43 @@ int main(){
     outf << strval;
 
     outf.close();
+
+    cout << "Read data from file - watch for errors." << endl;
+    string newstr;
+    ifstream inf;
+    inf.open("outdata");
+
+
+    if(inf){
+        int lucky2;
+        inf >> newstr >> lucky2;
+
+        if(lucky != lucky2){
+
+            cerr << "ERROR! wrong " << newstr << lucky2 << endl; 
+        }else{
+
+            cout << newstr << " OK" << endl;
+        }
+
+        float pi2;
+        inf >>newstr >> pi2;
+
+        if(pi != pi2)
+            cerr << "ERROR! wrong "<< newstr << pi2 << endl;
+        else
+            cout << newstr << " OK" << endl;
+   
+        double e2;
+
+        inf >> newstr >> e2;
+
+        if(e2 != e)
+            cerr << "ERROR! wrong " << newstr << e << endl;
+        else
+            cout << newstr << " OK" << endl;
+    }// end of IF 
+
+
 
 }
